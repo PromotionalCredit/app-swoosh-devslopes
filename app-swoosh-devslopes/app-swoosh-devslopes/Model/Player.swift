be8dffe4ea4afbdb5c9148ca0e5ec0ab6ec7af0f
Michael Czarnecki <mike@tuffbizz.com>
2019-01-22 13:35:08 -0500

    Added IBActions to LeagueVC to enable Next Button

---
+++ app-swoosh-devslopes/app-swoosh-devslopes/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh-devslopes
+//
+//  Created by Michael Czarnecki on 1/22/19.
+//  Copyright © 2019 Michael Czarnecki. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLever: String?
+}
